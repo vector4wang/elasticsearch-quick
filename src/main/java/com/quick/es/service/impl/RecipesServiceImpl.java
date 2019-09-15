1fd6c80eaf82348366c2cbde34e4f60ad39f67f8
vector <vector4wang@qq.com>
2019-09-15 20:57:21 +0800

    feat: 更新es的pom依赖

--- src/main/java/com/quick/es/service/impl/RecipesServiceImpl.java
+++ src/main/java/com/quick/es/service/impl/RecipesServiceImpl.java
@@ -19,7 +19,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
